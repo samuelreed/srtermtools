f2ce2cb46c59be17736135825ec517bacba368cc
Samuel Reed <1017865+samuelreed@users.noreply.github.com>
2020-09-06 12:34:27 -0700

    PEP8 cleanup.

--- routerrebooter.py
+++ routerrebooter.py
@@ -12,11 +12,11 @@ from termcolor import colored
 from bs4 import BeautifulSoup
 
 target = "192.168.0.1"
-proxies = {} # Useful for debugging to flip back and forth and read traffic
-#proxies = {'http': 'http://localhost:9191', 'https': 'http://localhost:9191'}
+proxies = {}  # Useful for debugging to flip back and forth and read traffic
+# proxies = {'http': 'http://localhost:9191', 'https': 'http://localhost:9191'}
 
-password = "" # TODO: research a console based secret store
-file = open('.dirty', 'r')
+password = ""  # TODO: research a console based secret store
+file = open(".dirty", "r")
 line = file.readlines()
 password = line[0]
 
@@ -24,7 +24,7 @@ password = line[0]
 def login():
     resp1 = requests.get("http://" + target, proxies=proxies)
     url = "http://" + target + "/goform/home_loggedout"
-    payload = {'loginUsername': 'admin', 'loginPassword': password}
+    payload = {"loginUsername": "admin", "loginPassword": password}
     resp2 = requests.post(url, cookies=resp1.cookies, data=payload, proxies=proxies)
     return resp1.cookies
 
@@ -32,60 +32,60 @@ def login():
 def reset(validcookies):
     resp3 = requests.get("http://" + target + "/restore_reboot.asp", cookies=validcookies, proxies=proxies)
     soup = BeautifulSoup(resp3.content, "html.parser")
-    csrftoken = soup.find('input', {'name': 'csrf_token'})['value']
-    payload = {'resetbt': '1', 'csrf_token': csrftoken}
+    csrftoken = soup.find("input", {"name": "csrf_token"})["value"]
+    payload = {"resetbt": "1", "csrf_token": csrftoken}
     resp4 = requests.post("http://" + target + "/goform/restore_reboot", cookies=validcookies, data=payload, proxies=proxies)
 
 
 def forwardingToken(validcookies):
     resp3 = requests.get("http://" + target + "/port_forwarding.asp", cookies=validcookies, proxies=proxies)
     soup = BeautifulSoup(resp3.content, "html.parser")
-    csrftoken = soup.find('input', {'name': 'csrf_token'})['value']
+    csrftoken = soup.find("input", {"name": "csrf_token"})["value"]
     return csrftoken
 
 
 def disableForwarding(validcookies):
     token = forwardingToken(validcookies)
-    payload = {'csrf_token': token, 'forwarding': 'Disabled'}
+    payload = {"csrf_token": token, "forwarding": "Disabled"}
     resp4 = requests.post("http://" + target + "/goform/port_forwarding", cookies=validcookies, data=payload, proxies=proxies)
 
 
 def enableForwarding(validcookies):
     token = forwardingToken(validcookies)
-    payload = {'csrf_token': token, 'forwarding': 'Enabled'}
+    payload = {"csrf_token": token, "forwarding": "Enabled"}
     resp4 = requests.post("http://" + target + "/goform/port_forwarding", cookies=validcookies, data=payload, proxies=proxies)
 
 
-if __name__ == '__main__':
-    args = docopt(__doc__, version='0.1')
+if __name__ == "__main__":
+    args = docopt(__doc__, version="0.1")
 
-    if(oct(os.stat(".dirty").st_mode & 0o777) != "0o600"):
-        print("[{}] Fix file permissions on .dirty file to 600.".format(colored(u'\u2717', "red")))
+    if oct(os.stat(".dirty").st_mode & 0o777) != "0o600":
+        print("[{}] Fix file permissions on .dirty file to 600.".format(colored(u"\u2717", "red")))
         exit()
 
-    if(args["check"]):
+    if args["check"]:
         try:
             login()
-            print("[{}] Verification of login was successful.".format(colored(u'\u2713', "green")))
+            print("[{}] Verification of login was successful.".format(colored(u"\u2713", "green")))
         except Exception as err:
-            print("[{}] Login to the router failed.".format(colored(u'\u2717', "red")))
-    elif(args["reset"]):
+            print("[{}] Login to the router failed.".format(colored(u"\u2717", "red")))
+    elif args["reset"]:
         try:
             reset(login())
-            print("[{}] Router reset.".format(colored(u'\u2713', "green")))
+            print("[{}] Router reset.".format(colored(u"\u2713", "green")))
         except Exception as err:
-            print("[{}] Resetting the router failed.".format(colored(u'\u2717', "red")))
-    elif(args["disable"]):
+            print("[{}] Resetting the router failed.".format(colored(u"\u2717", "red")))
+    elif args["disable"]:
         try:
             disableForwarding(login())
-            print("[{}] Port forwarding disabled.".format(colored(u'\u2713', "green")))
+            print("[{}] Port forwarding disabled.".format(colored(u"\u2713", "green")))
         except Exception as err:
-            print("[{}] Disabling forwarding failed.".format(colored(u'\u2717', "red")))
-    elif(args["enable"]):
+            print("[{}] Disabling forwarding failed.".format(colored(u"\u2717", "red")))
+    elif args["enable"]:
         try:
             enableForwarding(login())
-            print("[{}] Port forwarding enabled.".format(colored(u'\u2713', "green")))
+            print("[{}] Port forwarding enabled.".format(colored(u"\u2713", "green")))
         except Exception as err:
-            print("[{}] Enabling forwarding failed.".format(colored(u'\u2717', "red")))
+            print("[{}] Enabling forwarding failed.".format(colored(u"\u2717", "red")))
     else:
         print(docopt(__doc__))
--- sitecheck.py
+++ sitecheck.py
@@ -1,51 +1,52 @@
 #!/usr/bin/env python3
-'''Usage:
+"""Usage:
     sitecheck.py hash <url>
     sitecheck.py check <url> <lasthash>
-'''
+"""
 
 from docopt import docopt
 import ssdeep
 import requests
 from termcolor import colored
 
+
 def fetchpage(url):
     r = requests.get(url)
     status = r.status_code
-    newhash = ssdeep.hash( r.text )
+    newhash = ssdeep.hash(r.text)
     return status, newhash
 
-if __name__ == '__main__':
-    args = docopt(__doc__, version='0.1')
 
-    if( args["hash"] ):
-        status, newhash = fetchpage( args["<url>"] )
+if __name__ == "__main__":
+    args = docopt(__doc__, version="0.1")
+
+    if args["hash"]:
+        status, newhash = fetchpage(args["<url>"])
         print("[ {} ] for {}: {}".format(status, args["<url>"], newhash))
-    elif( args["check"] ):
+    elif args["check"]:
         catcherror = 0
         try:
-            status, newhash = fetchpage( args["<url>"] )
+            status, newhash = fetchpage(args["<url>"])
             verdict = "Verdict Never Set."
             color = "green"
-            compare = ssdeep.compare( newhash, args["<lasthash>"] )
-            if( compare == 0 ):
+            compare = ssdeep.compare(newhash, args["<lasthash>"])
+            if compare == 0:
                 verdict = "Site Changed Completely."
                 color = "magenta"
-            elif( compare == 100 ):
+            elif compare == 100:
                 verdict = "Site Unchanged."
                 color = "green"
-            elif( compare < 100 and compare >= 80 ):
+            elif compare < 100 and compare >= 80:
                 verdict = "Site Modified Slightly."
                 color = "yellow"
-            elif( compare < 80 and compare >= 50 ):
+            elif compare < 80 and compare >= 50:
                 verdict = "Site Modified Significantly."
                 color = "red"
-            elif( compare < 50 and compare > 0):
+            elif compare < 50 and compare > 0:
                 verdict = "Site Modified Heavily."
                 color = "magenta"
-            print("[ {} ] for {}: {}".format( colored(status,color), args["<url>"], colored(verdict,color)))
+            print("[ {} ] for {}: {}".format(colored(status, color), args["<url>"], colored(verdict, color)))
         except requests.ConnectionError:
             print("Unreachable site: {}".format(args["<url>"]))
     else:
-        print( docopt(__doc__) )
-
+        print(docopt(__doc__))
